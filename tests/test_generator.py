954db3e8a720783e6aecd3af437fbc3d1eb497f7
c1m50c <58411864+c1m50c@users.noreply.github.com>
2022-07-11 23:15:43 -0500

    `get_flags()` Test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,13 +1,31 @@
+from password_generator.flags import get_flags
 from password_generator.main import generate_password
 
 
 def test_password_generation() -> None:
     """
-    Tests the `app.main.generate_password()` method to ensure it
-    returns a string of proper length and contains the right set of characters.
+        Tests the `password_generator.main.generate_password()` method to ensure it
+        returns a string of proper length and contains the right set of characters.
     """
 
     password = generate_password("ab", 2)
 
     assert "a" in password or "b" in password
     assert len(password) == 2
+
+
+def test_get_flags() -> None:
+    """
+        Tests the `password_generator.flags.get_flags()` method to ensure it
+        returns the given flags and parameters in proper order.
+    """
+    
+    test_flags = "-c aAbBcC -qr ./output.bmp -copy -d 16".split(" ")
+    flags = get_flags(test_flags)
+    
+    assert flags == {
+        "-c": [ "aAbBcC" ],
+        "-qr": [ "./output.bmp" ],
+        "-copy": [  ],
+        "-d": [ "16" ],
+    }
